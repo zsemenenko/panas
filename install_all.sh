8f86e1609803d5276d978104d287bace13935b90
zsemenenko <z.semenenko@gmail.com>
2016-12-08 16:44:11 +0200

    modified install_all.sh and renamed install_docker_daemon.sh to docker_daemon_install.sh

--- install_all.sh
+++ install_all.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-$PWD/clear_db.sh &&
+$PWD/install_env.sh &&
+$PWD/docker_daemon_install.sh &&
+$PWD/node_install.sh &&
+$PWD/chrome_install.sh &&
+$PWD/hackrf_install.sh &&
+$PWD/osmosdr_install.sh &&
+$PWD/gqrx_install.sh &&
+$PWD/docker_services.sh &&
 $PWD/shortcuts.sh
 
 exit 0

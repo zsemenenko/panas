959a0520b3068ed77ce026e623b42ef5c366224c
zsemenenko <z.semenenko@gmail.com>
2016-12-08 13:45:18 +0200

    added ico dir; moved ico files to ico dir; added install_env.sh

---
+++ install_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update &&
+sudo apt-get install -y xinput-calibrator browser-plugin-freshplayer-pepperflash \
+git mumble-server mumble setserial minicom htop npm lm-sensors sysstat build-essential \
+cmake libusb-1.0-0-dev liblog4cpp5-dev libboost-dev libboost-system-dev libboost-thread-dev \
+libboost-program-options-dev swig qt5-default gnuradio gnuradio-dev gr-iqbal
+
+exit 0

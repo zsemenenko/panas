f2c7d300480394ca18a0b571650797bce544a4c0
zsemenenko <z.semenenko@gmail.com>
2017-01-04 15:11:06 +0200

    update docker_compose version 1.7 > 1.9

--- docker_daemon_install.sh
+++ docker_daemon_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo su && curl -fSsL https://get.docker.com/ | sh &&
-curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > ./docker-compose &&
+curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-Linux-x86_64 > ./docker-compose &&
 chmod +x ./docker-compose && sudo cp docker-compose /usr/local/bin/ &&
 sudo usermod -aG docker $(whoami)
  

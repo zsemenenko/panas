2b7e15773ea12a8582a80c8bfddd07be2bf46ee7
zsemenenko <z.semenenko@gmail.com>
2017-01-04 15:27:17 +0200

    privileges level fix

--- docker_daemon_install.sh
+++ docker_daemon_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo su && curl -fSsL https://get.docker.com/ | sh &&
+curl -fSsL https://get.docker.com/ | sh &&
 curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-Linux-x86_64 > ./docker-compose &&
 chmod +x ./docker-compose && sudo cp docker-compose /usr/local/bin/ &&
 sudo usermod -aG docker $(whoami)
